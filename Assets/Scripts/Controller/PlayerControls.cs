40730deb07d1ed027a58844567ed55b0dc135e97
Hussain <h.alkhorsan@gmail.com>
2021-10-30 22:19:35 +0100

    Organized the project directories

--- Assets/Scripts/Controller/PlayerControls.cs
+++ Assets/Scripts/Controller/PlayerControls.cs
@@ -2,32 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class PlayerControls : MonoBehaviour
+{ 
     void Start()
     {
-
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move();
     }
 
     public float MovementSpeed;
+    public float JumpPower;
 
     public void Move()
     {
+
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(MovementSpeed, 0, 0);
+            transform.Translate(-MovementSpeed, 0, 0);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-MovementSpeed, 0, 0);
+            transform.Translate(MovementSpeed, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(transform.up * JumpPower);
         }
     }
 }
