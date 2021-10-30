05608bfaa0db86798bd7b45a186672128bfc4dd5
Hussain <h.alkhorsan@gmail.com>
2021-10-30 02:05:49 +0100

    Implemented health bar indicator for both players

---
+++ Assets/Player1Health.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Health : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        // Set player current health to max health
+        currentHealth = maxHealth;
+
+        // Set health bar to max health
+        healthBar.SetMaxHealth(maxHealth); 
+    }
+
+    void Update()
+    {
+        // Testing
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            //Debug.Log("Space key was pressed.");
+            TakeDamage(10);
+        }
+    }
+
+    // If player has been hit, decrease player health
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
