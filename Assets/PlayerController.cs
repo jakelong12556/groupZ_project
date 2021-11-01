5e614b562aa51a5ec96590d750820a07f5ec8d91
Phan Minh Cuong <Jakelong12556@yahoo.com>
2021-11-01 22:44:55 +0000

    movement animation network and a combo system

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public static PlayerController instance;
+    public Animator AnimController;
+
+    public bool atkPressed;
+
+    public bool canReceiveInput;
+    public bool inputReceived;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-
+        //atkPressed = false;
+        inputReceived = false;
+        canReceiveInput = true;
     }
 
-    public float MovementSpeed;
-    public Animator AnimController;
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Attack();
+        // if (Input.GetKey(KeyCode.F) != true || atkPressed == false)
+        // {
+        //     Attack();
+        // }
+        // else
+        // {
+        //     Debug.Log("punch_1_cancelled");
+        //     AnimController.SetBool("attack1", false);
+        // }
     }
 
 
@@ -24,24 +49,78 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.D))
         {
-            //transform.Translate(MovementSpeed, 0, 0);
-            //AnimController.Play("Forward");
-            AnimController.SetFloat("right", 0.1f);
+            AnimController.SetBool("forward", true);
         }
         else
         {
-            AnimController.SetFloat("right", -0.1f);
+            AnimController.SetBool("forward", false);
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            //transform.Translate(-MovementSpeed, 0, 0);
-            //AnimController.Play("Back");
-            AnimController.SetFloat("left", 0.1f);
+            AnimController.SetBool("backward", true);
         }
         else
         {
-            AnimController.SetFloat("left", -0.1f);
+            AnimController.SetBool("backward", false);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            AnimController.SetBool("crouch", true);
+        }
+        else
+        {
+            AnimController.SetBool("crouch", false);
         }
     }
+    public void Attack()
+    {
+        // if (Input.GetKey(KeyCode.F))
+        // {
+        //     if (AnimController.GetCurrentAnimatorStateInfo(0).IsName("Punch_1"))
+        //     {
+        //         Debug.Log("punch_1_cancelled");
+        //         AnimController.SetBool("attack1", false);
+        //     }
+        //     else
+        //     {
+        //         AnimController.SetBool("attack1", true);
+        //         atkPressed = true;
+        //     }
+        // }
+        // else
+        // {
+        //     AnimController.SetBool("attack1", false);
+        //     atkPressed = false;
+        // }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (canReceiveInput)
+            {
+                inputReceived = true;
+                canReceiveInput = false;
+            }
+            else
+            {
+                Debug.Log("no punch");
+                return;
+            }
+        }
+    }
+    public void InputManager()
+    {
+        if (!canReceiveInput)
+        {
+            canReceiveInput = true;
+        }
+        else
+        {
+            canReceiveInput = false;
+        }
+
+    }
 }
+
+
+
