3a7280ff15eb75d65d5c4804146d070c9d900124
Hussain <h.alkhorsan@gmail.com>
2021-11-12 02:00:07 +0000

    evaulate players health on time out and display winning player

---
+++ Assets/AIPlayer.cs
@@ -0,0 +1,225 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIPlayer : MonoBehaviour
+{
+
+    public static AIPlayer instance;
+    public Animator AnimController;
+    public bool canReceiveInput;
+    public bool inputReceived;
+    public bool inputReceived2;
+    public bool inputReceived3;
+
+    Dictionary<int, System.Action> moves = new Dictionary<int, System.Action>();
+    //moves.Add(1, MoveForward);
+    //moves.Add(2, MoveBackward);
+    //moves.Add(3, Crouch);
+    //moves.Add(4, Stand);
+    //moves.Add(5, Attack);
+
+    System.Random random = new System.Random();
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //System.Random random = new System.Random();
+        //int num = random.Next(1, 5);
+       // moves.Add(1, MoveForward);
+       // moves.Add(2, MoveBackward);
+        //moves.Add(3, Crouch);
+        moves.Add(4, Stand);
+        moves.Add(5, Attack1);
+        moves.Add(6, Attack2);
+        moves.Add(7, Attack3);
+
+        canReceiveInput = true;
+        inputReceived = false;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        int num = random.Next(1, 7);
+
+        switch (num)
+        {
+          //  case 1:
+          //      moves[1]();
+          //      break;
+
+         //   case 2:
+          //      moves[2]();
+          //      break;
+            //case 3:
+              //  moves[3]();
+                //break;
+            case 4:
+                moves[4]();
+                break;
+            case 5:
+                moves[5]();
+                break;
+            case 6:
+                moves[6]();
+                break;
+            case 7:
+                moves[7]();
+                break;
+
+            default:
+                break;
+        }
+
+    }
+
+
+    public void MoveForward()
+    {
+        AnimController.SetBool("forward", true);
+    }
+
+    public void MoveBackward()
+    {
+        AnimController.SetBool("forward", false);
+    }
+
+    public void Crouch()
+    {
+        AnimController.SetBool("crouch", true);
+    }
+
+    public void Stand()
+    {
+        AnimController.SetBool("crouch", false);
+    }
+
+    public void Attack1()
+    {
+        if (canReceiveInput)
+        {
+            inputReceived = true;
+            canReceiveInput = false;
+
+        }
+
+    }
+
+    public void Attack2()
+    {
+        if (canReceiveInput)
+        {
+            inputReceived2 = true;
+            canReceiveInput = false;
+
+        }
+
+    }
+    
+    public void Attack3()
+    {
+        if (canReceiveInput)
+        {
+            inputReceived3 = true;
+            canReceiveInput = false;
+
+        }
+
+    }   
+}
+
+
+    //public void Move()
+    //{
+    //    if (Input.GetKey(KeyCode.J))
+    //    {
+    //        AnimController.SetBool("forward", true);
+    //    }
+    //    else
+    //    {
+    //        AnimController.SetBool("forward", false);
+    //    }
+
+    //    if (Input.GetKey(KeyCode.L))
+    //    {
+    //        AnimController.SetBool("backward", true);
+    //    }
+    //    else
+    //    {
+    //        AnimController.SetBool("backward", false);
+    //    }
+    //    if (Input.GetKey(KeyCode.K))
+    //    {
+    //        AnimController.SetBool("crouch", true);
+    //    }
+    //    else
+    //    {
+    //        AnimController.SetBool("crouch", false);
+    //    }
+    //}
+    //public void Attack()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.H) && Input.GetKey(KeyCode.K))
+    //    {
+    //        if (canReceiveInput)
+    //        {
+    //            inputReceived3 = true;
+    //            canReceiveInput = false;
+    //        }
+    //        else
+    //        {
+    //            Debug.Log("no sweep");
+    //            return;
+    //        }
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.H))
+    //    {
+    //        if (canReceiveInput)
+    //        {
+    //            inputReceived = true;
+    //            canReceiveInput = false;
+    //        }
+    //        else
+    //        {
+    //            Debug.Log("no punch");
+    //            return;
+    //        }
+    //    }
+    //    if (Input.GetKeyDown(KeyCode.Y))
+    //    {
+    //        if (canReceiveInput)
+    //        {
+    //            inputReceived2 = true;
+    //            canReceiveInput = false;
+    //        }
+    //        else
+    //        {
+    //            Debug.Log("no punch2");
+    //            return;
+    //        }
+    //    }
+
+    //}
+  //  public void InputManager()
+    //{
+     //   if (!canReceiveInput)
+      //  {
+       //     canReceiveInput = true;
+        //}
+        //else
+        //{
+         //   canReceiveInput = false;
+        //}
+
+    //}
+//}
+
+
+
--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -5,9 +5,13 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
+    public Player1Health player1Health;
+    public Player2Health player2Health;
+
     float currentTime = 0f;
-    float startingTime = 60f;
+    float startingTime = 10f;
     [SerializeField] Text countdownText;
+    [SerializeField] Text messageText;
 
     // Start is called before the first frame update
     void Start()
@@ -23,11 +27,29 @@ public class Timer : MonoBehaviour
         {
 
             countdownText.text = currentTime.ToString("0");
+            messageText.text = "";
         }
 
         if (currentTime <= 0)
+
+           
+
         {
             currentTime = 0;
+
+            if (player1Health.currentHealth > player2Health.currentHealth) 
+            {
+                messageText.text = "PLAYER 1 WINS!";
+            }
+            else if (player1Health.currentHealth < player2Health.currentHealth) 
+            {
+                messageText.text = "PLAYER 2 WINS!";
+            }
+            else 
+            {
+                messageText.text = "DRAW!";
+            }
+ 
         }
 
     }
